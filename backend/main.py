037e259751f3585fc4a4f13e801071efba27c03b
sihyeong671 <bshlab671@naver.com>
2023-03-07 14:13:05 +0900

    feat: add jinja template
    - home 템플릿 추가
    - main에서 들어갈 수 있도록 api 추가

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 import uvicorn
 from api import api_router
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+template = Jinja2Templates(directory="templates")  # terminal 기준 path
 
 app.include_router(api_router, prefix="/api")
 
@@ -10,6 +14,11 @@ app.include_router(api_router, prefix="/api")
 @app.on_event("startup")
 def start():
     pass
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return template.TemplateResponse("home.html", context={"request": request})
 
 
 if __name__ == "__main__":
