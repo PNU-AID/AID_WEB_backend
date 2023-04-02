cce44aacd393e35f055a4147e3ec022716d631ed
jmk445 <jmk445123456@gmail.com>
2023-04-02 18:19:44 +0900

    이해한 내용을 주석으로 추가

--- backend/api/endpoint/submit.py
+++ backend/api/endpoint/submit.py
@@ -5,23 +5,25 @@ from fastapi.templating import Jinja2Templates
 from backend.crud import create_submit
 from backend.scheme.submit import SubmitForm
 
-router = APIRouter()
+##submit에 관한 api를 담은 코드
 
-template = Jinja2Templates(directory="templates")  # terminal 기준 path
+router = APIRouter() #submit ROUTER 를 선언해준다. 
+
+template = Jinja2Templates(directory="templates")  # terminal 기준 path 
 
 
-@router.get("", response_class=HTMLResponse)
+@router.get("", response_class=HTMLResponse) #get request (http request)
 def submit_page(request: Request):
     return template.TemplateResponse("submit.html", context={"request": request})
 
 
-@router.post("")
+@router.post("") #post request(http request)
 async def submit(request: Request):
     form = SubmitForm(request)
     await form.load_data()
 
-    if form.is_valid():
-        create_submit(form.send_data())
-        return responses.RedirectResponse("/?msg=success", status_code=status.HTTP_302_FOUND)
+    if form.is_valid(): #form이 valid 하면 
+        create_submit(form.send_data()) #crud폴더의 create_submit 함수 
+        return responses.RedirectResponse("/?msg=success", status_code=status.HTTP_302_FOUND) #
 
-    return template.TemplateResponse("submit.html", context=form.__dict__)
+    return template.TemplateResponse("submit.html", context=form.__dict__) #jinja template을 return
--- backend/crud/submit.py
+++ backend/crud/submit.py
@@ -1,12 +1,14 @@
-def create_submit(data):
+#submit에 관한 crud 구현
+
+def create_submit(data): #submit을 생성(submit form을 생성?), api\endpoint\submit api에서 사용함
     # db['submit'].insert_one(data)
     print(type(data))
     print(data)
 
 
-def read_all_submit(data):
+def read_all_submit(data): #submit을 모두 읽음
     pass
 
 
-def read_submit(data):
+def read_submit(data): #submit을 하나 읽어옴
     pass
