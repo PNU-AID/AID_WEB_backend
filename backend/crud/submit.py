71146ed3052f7ca339e8c461266295c657ebe2aa
stop._.bmin <geemin0@naver.com>
2023-04-09 16:19:44 +0900

    merge submit & upstream

--- backend/crud/submit.py
+++ backend/crud/submit.py
@@ -1,14 +1,16 @@
-#submit에 관한 crud 구현
+# submit에 관한 crud 구현
+from backend.database import db
 
-def create_submit(data): #submit을 생성(submit form을 생성?), api\endpoint\submit api에서 사용함
-    # db['submit'].insert_one(data)
+
+def create_submit(data):  # submit을 생성(submit form을 생성?), api\endpoint\submit api에서 사용함
+    db["submit"].insert_one(data)
     print(type(data))
     print(data)
 
 
-def read_all_submit(data): #submit을 모두 읽음
+def read_all_submit(data):  # submit을 모두 읽음
     pass
 
 
-def read_submit(data): #submit을 하나 읽어옴
+def read_submit(data):  # submit을 하나 읽어옴
     pass
