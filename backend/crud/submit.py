e688bf295b7f9d84eea1dcdd778e0f743d7b42cc
jmk445 <jmk445123456@gmail.com>
2023-04-09 11:43:45 +0900

    0409_readSubmit

--- backend/crud/submit.py
+++ backend/crud/submit.py
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
-#submit에 관한 crud 구현
-
-def create_submit(data): #submit을 생성(submit form을 생성?), api\endpoint\submit api에서 사용함
-    # db['submit'].insert_one(data)
-    print(type(data))
-    print(data)
-
-
-def read_all_submit(data): #submit을 모두 읽음
-    pass
-
-
-def read_submit(data): #submit을 하나 읽어옴
-=======
 from backend.database import db
-
+#submit에 관한 crud 구현
 
 def create_submit(data: dict):
     db["submit"].insert_one(data)
 
 
-def read_all_submit():
-    # TODO
-    pass
+def read_all_submit(data): #submit을 모두 읽음
+    all_submits = db.submit.find()
+    return all_submits
 
 
-def read_submit(_id: str):
-    # TODO
->>>>>>> 5612cb7c293d543902942a9dcc91754dbcd68913
-    pass
+def read_submit(data): #submit을 하나 읽어옴
+    submits = db.submit.find_one({"id":id})
+    return submits
