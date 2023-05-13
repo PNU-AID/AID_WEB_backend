5a92022cc5e689ff4ff6c6d93f3e4289195a4ebb
stopmin <geemin0@naver.com>
2023-05-14 01:10:44 +0900

    Add login.html & login_router

--- backend/api/endpoint/__init__.py
+++ backend/api/endpoint/__init__.py
@@ -1,3 +1,4 @@
 from .admin import router as admin_router  # noqa
 from .auth import router as auth_router  # noqa
+from .login import router as login_router  # noqa
 from .submit import router as submit_router  # noqa
