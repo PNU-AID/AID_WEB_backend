ee1c8abe5534799565bee34678169b8328b78dbe
jmk445 <jmk445123456@gmail.com>
2023-05-16 11:39:44 +0900

    test

--- backend/api/endpoint/admin.py
+++ backend/api/endpoint/admin.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from backend.crud.submit import read_all_submit, read_submit
-from backend.database import db
+from backend.crud.submit import get_count, read_all_submit, read_submit
 
 router = APIRouter()
 
@@ -15,14 +14,14 @@ def submission_list_page(request: Request, submit_page: int = 1):
     """admin 페이지 반환"""
 
     all_data = read_all_submit()
-    all_data_cnt = db["submit"].count_documents()
+    all_data_cnt = get_count()
     # TODO
     limit = 3
     offset = (submit_page - 1) * limit
     selected = all_data.limit(limit).skip(offset)
 
     return template.TemplateResponse(
-        "submission_list.html", context={"request": request, "submission": selected}, values=all_data_cnt
+        "submission_list.html", context={"request": request, "submission": selected, "values": all_data_cnt}
     )
 
 
