f0a855f5bdc66d5374ea8cf6b263f81c980c514a
jmk445 <jmk445123456@gmail.com>
2023-08-06 00:26:58 +0900

    feat : submit api delete 코드 추가, sign_up test코드 추가 - submit api delete 기능 추가, delete_submit 함수 추가 - pyproject.toml env 변수 추가(13~16),user_test.py 작성

--- app/test/user_test.py
+++ app/test/user_test.py
@@ -9,5 +9,5 @@ client = TestClient(app)
 
 def test_sign_up():
     response = client.post("/signup", json={"email": "test@example.com", "password": "password"})
-    assert response.status_code == 404
+    assert response.status_code == 200
     assert response.json() == {"email": "test@example.com", "password": "password"}
