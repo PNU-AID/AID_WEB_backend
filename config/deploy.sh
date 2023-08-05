f81341db81b0687f78757f8a050983692937800e
sihyeong671 <bshlab671@naver.com>
2023-08-06 04:07:23 +0900

    fix: deploy.sh 파일 수정

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,30 +1,11 @@
 # !/bin/bash
 
-# docker가 없다면, docker 설치
-# if ! type docker > /dev/null
-# then
-#   echo "docker does not exist"
-#   echo "Start installing docker"
-#   sudo apt-get update
-#   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-#   sudo apt update
-#   apt-cache policy docker-ce
-#   sudo apt install -y docker-ce
-# fi
-
-# docker-compose가 없다면 docker-compose 설치
-# if ! type docker-compose > /dev/null
-# then
-#   echo "docker-compose does not exist"
-#   echo "Start installing docker-compose"
-#   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#   sudo chmod +x /usr/local/bin/docker-compose
-# fi
+# docker 설치
+# docker compose 설치
+# dockerhub login
 
 echo "start docker-compose up: ubuntu"
 sudo docker compose -f docker-compose.prod.yaml down
-sudo docker rmi ${{ secrets.DOCKERHUB_ID }}/aid_web
-sudo docker pull ${{ secrets.DOCKERHUB_ID }}/aid_web
+sudo docker rmi bshlab671/aid_web
+sudo docker pull bshlab671/aid_web
 sudo docker compose -f docker-copmose.prod.yaml up -d
