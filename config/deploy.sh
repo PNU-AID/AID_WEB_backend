827817afae90d1a16bb5dfadbdb60ea8d4002760
sihyeong671 <bshlab671@naver.com>
2023-08-06 04:09:55 +0900

    chore: fix typo

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,6 +6,7 @@
 
 echo "start docker-compose up: ubuntu"
 sudo docker compose -f docker-compose.prod.yaml down
+# image 있는지 확인(err 뜨지만 멈추지는 않음)
 sudo docker rmi bshlab671/aid_web
 sudo docker pull bshlab671/aid_web
-sudo docker compose -f docker-copmose.prod.yaml up -d
+sudo docker compose -f docker-compose.prod.yaml up -d
