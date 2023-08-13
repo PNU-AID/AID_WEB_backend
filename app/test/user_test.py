b24b424189d74ccfd16b533c8e1dbadb5c7e7e1b
jmk445 <jmk445123456@gmail.com>
2023-08-14 00:05:52 +0900

    test

--- app/test/user_test.py
+++ app/test/user_test.py
@@ -10,4 +10,4 @@ client = TestClient(app)
 def test_sign_up():
     response = client.post("/signup", json={"email": "test@example.com", "password": "password"})
     assert response.status_code == 200
-    assert response.json() == {"email": "test@example.com", "password": "password"}
+    assert response.json() == {"msg": "user created"}
