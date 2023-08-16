5ccf7546b3193b5d6308e17a02e2d27b2ad177a4
sihyeong671 <bshlab671@naver.com>
2023-08-17 03:36:44 +0900

    refactor: submit function 수정 및 예외처리 추가
    - model dump로 crud 코드 넘어가기전 json변환해서 넘기도록 코드 수정
      - json 내부 값 수정 및  연산은 crud 코드에서 하도록하기 위해 변경
    - submit api 예외 처리 추가

--- app/backend/crud/submit.py
+++ app/backend/crud/submit.py
@@ -1,25 +1,46 @@
 from backend.database import db_manager
-from backend.scheme.submit import SubmitForm
 from bson import ObjectId
 
 
-def create_submit(submit_data: SubmitForm) -> str:
-    submit_input = db_manager.db.submit.insert_one(submit_data.dict())
-    return str(submit_input.inserted_id)
+def create_submit(submit_data: dict) -> str:
+    """지원서 저장
+
+    Args:
+        submit_data (dict)
+
+    Returns:
+        str: str type object id
+    """
+    result = db_manager.db.submit.insert_one(submit_data)
+    return str(result.inserted_id)
 
 
-def read_submit(submit_id: str) -> dict:
+def read_submit(submit_id: str) -> dict | None:
+    """지원서 가져오기
+
+    Args:
+        submit_id (str)
+
+    Returns:
+        dict: submit form 내용
+    """
     submit = db_manager.db.submit.find_one({"_id": ObjectId(submit_id)})
     return submit
 
 
-def update_submit(submit_id: str, submit_data: SubmitForm):
-    db_manager.db.submit.update_one({"_id": ObjectId(submit_id)}, {"$set": submit_data.dict()})
+def update_submit(submit_id: str, submit_data: dict):
+    """지원서 수정
+
+    Args:
+        submit_id (str)
+        submit_data (dict)
+    """
+    # 없는 id 입력될 시 에러
+    db_manager.db.submit.update_one({"_id": ObjectId(submit_id)}, {"$set": submit_data})
 
 
 def delete_submit(submit_id: str):
-    # TODO
-    # make this part
+    # 없는 id 입력될 시 에러
     db_manager.db.submit.delete_one({"_id": ObjectId(submit_id)})
 
 
