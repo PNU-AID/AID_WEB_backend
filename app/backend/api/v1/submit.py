fd875058d8d8f6211691cfe17011d08e033d4e09
sihyeong671 <bshlab671@naver.com>
2023-08-16 15:07:29 +0900

    feat: docs 설명추가, comment scheme추가

--- app/backend/api/v1/submit.py
+++ app/backend/api/v1/submit.py
@@ -14,19 +14,39 @@ router = APIRouter()  # submit ROUTER 를 선언해준다.
 
 @router.post("/create")
 def upload_submit(submission: SubmitForm):
+    """지원자들이 지원하는 submit 제출하는 api<br/>
+    Args:
+    - (SubmitForm)
+
+    Returns:<br/>
+    - id: str 타입의 mongodb object id
+    """
     inserted_id = create_submit(submission)
     return JSONResponse(content={"id": inserted_id}, status_code=status.HTTP_201_CREATED)
 
 
-@router.get("/read")
+@router.get("/read", response_model=SubmitForm)
 def get_my_submit(submit_id: str = Query()):
+    """본인이 가진 id로 제출한 지원서 가져오는 api<br/>
+
+    Args:
+    - submit_id (str): mongodb id
+
+    Returns:
+    - (SubmitForm)
+    """
     submit = read_submit(submit_id)
-    del submit["_id"]
     return submit
 
 
 @router.put("/modify")
 def modify_my_submit(submit_id: str, submission: SubmitForm):
+    """본인의 지원서 수정하는 api<br/>
+
+    Args:
+    - submit_id (str)
+    - submission (SubmitForm)
+    """
     update_submit(submit_id, submission)
 
 
--- app/backend/scheme/question_board.py
+++ app/backend/scheme/question_board.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from backend.core.utils import ObjectIdStr
+from backend.core.utils import ObjectIdStr, StrObjectId
 from pydantic import BaseModel, Field
 
 # 위 코드는 Pydantic을 사용하여 MongoDB에서 사용될 "User 데이터 모델과 관련된 클래스"들을 정의하고 있다.
@@ -9,21 +9,24 @@ from pydantic import BaseModel, Field
 class QuestionBase(BaseModel):
     title: str = Field(...)
     content: str = Field(...)
+    comments: list = []
 
 
 class QuestionIn(QuestionBase):
     created_time: datetime = Field(default_factory=datetime.now)
-    comments: list = []
 
 
 class QuestionOut(QuestionBase):
     id: ObjectIdStr = Field(alias="_id")
     created_time: datetime
-    comments: list = []
 
     class Config:
         arbitrary_types_allowed = True
 
 
-# TODO
-# comment
+class CommentIn(BaseModel):
+    question_id: StrObjectId = Field(...)
+    content: str = Field(...)
+
+    class Config:
+        arbitrary_types_allowed = True
