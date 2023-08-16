4c5a25e36733f280f370ac45c7d11b8c237ed201
jmk445 <jmk445123456@gmail.com>
2023-08-16 15:05:24 +0900

    remove : pytest코드 삭제

--- app/test/user_test.py
+++ app/test/user_test.py
@@ -5,23 +5,3 @@ client = TestClient(app)
 
 # TODO
 # make api test function
-
-
-def test_sign_up():
-    response = client.post(
-        "/create",
-        json={
-            "name": "asdf",
-            "email": "EmailStr",
-            "student_id": "asdf",
-            "phone_number": "str",
-            "motivation": "str",
-            "github": "foo",
-            "blog": "bar",
-            "ai_exp": "foo",
-            "personal_info_agree": "true",
-        },
-    )
-
-    assert response.status_code == 201
-    assert response.json() == {"id": "Foo"}
