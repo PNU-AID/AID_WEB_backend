b9b1d93b58ed31e2fa6004cade0e2dddcbb96869
JongMok Lee <lijm1358@gmail.com>
2023-11-03 13:28:13 +0900

    chore: test server deploy용 docker, deploy shell

---
+++ config/deploy_test.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# docker 설치
+# docker compose 설치
+# dockerhub login
+
+echo "start docker-compose up: ubuntu"
+sudo docker compose -f docker-compose.test.yaml down
+# image 있는지 확인(err 뜨지만 멈추지는 않음)
+sudo docker rmi bshlab671/aid_web
+sudo docker pull bshlab671/aid_web
+sudo docker compose -f docker-compose.test.yaml up -d
